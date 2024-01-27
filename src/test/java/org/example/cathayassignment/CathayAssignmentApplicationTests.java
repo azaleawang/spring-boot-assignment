eefd234005842ce8b2e70a3471c2e63a8a272366
azalea <azaleawang4@gmail.com>
2024-01-27 15:38:33 +0800

    First commit

---
+++ src/test/java/org/example/cathayassignment/CathayAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cathayassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathayAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
